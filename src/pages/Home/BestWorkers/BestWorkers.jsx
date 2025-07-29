aabc6aca88c50f3f10ecd7c30d7739098fe49e4a
RGAntu <rgantu887@gmail.com>
2025-07-29 19:35:50 +0600

    added pagination

--- src/pages/Home/BestWorkers/BestWorkers.jsx
+++ src/pages/Home/BestWorkers/BestWorkers.jsx
@@ -20,7 +20,7 @@ const BestWorkers = () => {
   return (
     <div className="max-w-5xl mx-auto p-4">
       <h2 className="text-3xl font-bold mb-6 text-center">Top Performing Workers</h2>
-      <p className="text-accent text-center">Meet our workers who have earned the most through completing tasks</p>
+      <p className="text-accent text-center mb-5">Meet our workers who have earned the most through completing tasks</p>
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-6 gap-4">
         {workers.map((worker) => (
           <div
