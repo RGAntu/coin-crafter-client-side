d6081a8d3165837cb3c56b42e964d880360720b0
RGAntu <rgantu887@gmail.com>
2026-02-01 18:03:41 +0600

    modified bg color & btn

--- src/pages/ForWorkers/StartEarning/StartEarning.jsx
+++ src/pages/ForWorkers/StartEarning/StartEarning.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router";
 
 const StartEarning = () => {
   return (
-    <section className="py-20 bg-gradient-to-r from-blue-500 to-teal-400 text-center text-white">
-      <h2 className="text-3xl font-bold mb-4">Ready to Start Earning?</h2>
-      <p className="text-lg mb-6">
+    <section className="py-20 bg-gradient-main text-center text-white">
+      <h2 className="text-4xl font-bold mb-4">Ready to Start Earning?</h2>
+      <p className="text-xl mb-6">
         Sign up today and get 10 free coins to start your earning journey!
       </p>
-     
+
       <Link to="/register">
-  <button className="btn bg-accent hover:bg-secondary border-none text-white font-semibold px-6 py-6 rounded-lg shadow-md hover:scale-105 transition duration-300">
-    Create Worker Account
-  </button>
-</Link>
+        <button className="btn bg-color-text hover:bg-secondary border-none text-color-accent hover:text-white font-semibold px-6 py-6 rounded-lg shadow-md hover:scale-105 transition duration-300">
+          Create Worker Account
+        </button>
+      </Link>
     </section>
   );
 };
