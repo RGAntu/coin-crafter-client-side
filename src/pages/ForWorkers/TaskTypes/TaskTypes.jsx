17f2627dc5fa54fec6c63d29c8c910c9c0b726fe
RGAntu <rgantu887@gmail.com>
2026-02-01 17:09:00 +0600

    modified task-types & added icons, animation

--- src/pages/ForWorkers/TaskTypes/TaskTypes.jsx
+++ src/pages/ForWorkers/TaskTypes/TaskTypes.jsx
@@ -1,15 +1,48 @@
 import React from "react";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import { TfiWrite } from "react-icons/tfi";
+import { IoPhonePortrait } from "react-icons/io5";
+import { RiSurveyLine } from "react-icons/ri";
+import { MdReviews } from "react-icons/md";
+import { BiSolidVideos } from "react-icons/bi";
+import { AiOutlineTranslation } from "react-icons/ai";
+import { MdAssistant } from "react-icons/md";
+import { motion } from "motion/react";
 
 const TaskTypes = () => {
   const tasks = [
-    "Data Entry & Research",
-    "Content Writing & Editing",
-    "Social Media Engagement",
-    "Survey Participation",
-    "Product Reviews",
-    "Image & Video Tasks",
-    "Translation Services",
-    "Virtual Assistance",
+    {
+      title: "Data Entry & Research",
+      icons: <LuFileSpreadsheet className="text-[#2688d9]" />,
+    },
+    {
+      title: "Content Writing & Editing",
+      icons: <TfiWrite className="text-[#2688d9]" />,
+    },
+    {
+      title: "Social Media Engagement",
+      icons: <IoPhonePortrait className="text-[#2688d9]" />,
+    },
+    {
+      title: "Survey Participation",
+      icons: <RiSurveyLine className="text-[#2688d9]" />,
+    },
+    {
+      title: "Product Reviews",
+      icons: <MdReviews className="text-[#2688d9]" />,
+    },
+    {
+      title: "Image & Video Tasks",
+      icons: <BiSolidVideos className="text-[#2688d9]" />,
+    },
+    {
+      title: "Translation Services",
+      icons: <AiOutlineTranslation className="text-[#2688d9]" />,
+    },
+    {
+      title: "Virtual Assistance",
+      icons: <MdAssistant className="text-[#2688d9]" />,
+    },
   ];
 
   return (
@@ -27,12 +60,18 @@ const TaskTypes = () => {
         {/* Task Boxes */}
         <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {tasks.map((task, index) => (
-            <div
+            <motion.div
+              whileHover={{ scale: 1.03 }}
+              whileTap={{ scale: 0.1 }}
+              onHoverStart={() => console.log("hover started!")}
               key={index}
-              className="bg-white py-4 px-6 rounded-lg shadow-sm border text-gray-800 font-medium hover:shadow-md transition cursor-pointer"
+              className="bg-white py-4 px-6 rounded-lg shadow-sm border border-[#f7f7f7] text-gray-800 font-medium hover:shadow-md transition cursor-pointer"
             >
-              {task}
-            </div>
+              <div className="flex flex-col justify-center gap-5">
+                <div className=" text-4xl block mx-auto ">{task.icons}</div>
+                <h2 className="text-base font-semibold">{task.title}</h2>
+              </div>
+            </motion.div>
           ))}
         </div>
       </div>
